40eb536b0234bfa4dc7d495c39f5f6c70e635f68
usermisterfive <usermisterfive@yandex.ru>
2025-02-23 20:07:32 +0400

    #4 do not remove button icon , instead set another icon

--- src/main/java/com/usermisterfive/Base.java
+++ src/main/java/com/usermisterfive/Base.java
@@ -10,82 +10,88 @@ import java.util.Objects;
 
 public class Base {
  private static final String host = "1.1.1.1";
+ private boolean running;
  public static void main(String[] args) {
-  while (true) {
-   TrayIcon trayIcon = setIcon2();
+  Base base = new Base();
+  base.setRunning(true);
+
+  TrayIcon trayIcon = new TrayIcon(Objects.requireNonNull(
+    createImage("unknown.png")));
+  try {
+   SystemTray.getSystemTray().add(trayIcon);
+  } catch (AWTException awtException) {
+   System.out.println(awtException);
+  }
+
+  while (base.isRunning()) {
    try {
     Thread.sleep(1000);
    } catch(InterruptedException interruptedException) {
     System.out.println(interruptedException);
    }
-   SystemTray.getSystemTray().remove(trayIcon);
+   testAndAssignImage(trayIcon);
 
   }
  }
  static boolean test(int timeout) {
-  boolean isSuccessful = false;
+  boolean reachable = false;
   try {
-   String isReachable = "";
-   isSuccessful = InetAddress.getByName(host).isReachable(timeout);
-   if (!isSuccessful) {
-    isReachable = "not ";
+   String reachableWord = "";
+   reachable = InetAddress.getByName(host).isReachable(timeout);
+   if (!reachable) {
+    reachableWord = "not ";
    }
-   System.out.println(host + " is " + isReachable + "reachable within "
+   System.out.println(host + " is " + reachableWord + "reachable within "
      + timeout + " ms");
 
   } catch(IOException ioException) {
    System.out.println(ioException);
   }
-  return isSuccessful;
+  return reachable;
  }
- private static Image createImage(String path, String description) {
+ private static Image createImage(String path) {
   URL imageURL = Base.class.getClassLoader().getResource(path);
 
   if (imageURL == null) {
    System.err.println("Resource not found: " + path);
    return null;
   } else {
-   return (new ImageIcon(imageURL, description)).getImage();
+   return (new ImageIcon(imageURL, "")).getImage();
   }
  }
- private static TrayIcon setIcon(String path) {
-  TrayIcon trayIcon = new TrayIcon(Objects.requireNonNull(
-    createImage(path, "")));
-  try {
-   SystemTray.getSystemTray().add(trayIcon);
-  } catch (AWTException awtException) {
-   System.out.println(awtException);
-  }
-  return trayIcon;
-
- }
- private static TrayIcon setIcon2() {
-  TrayIcon trayIcon;
+ private static void testAndAssignImage(TrayIcon trayIcon) {
   if (test(1)) {
-   trayIcon = setIcon("1.png");
+   trayIcon.setImage(createImage("1.png"));
   }
   else if (test(2)) {
-   trayIcon = setIcon("2.png");
+   trayIcon.setImage(createImage("2.png"));
   }
   else if (test(3)) {
-   trayIcon = setIcon("3.png");
+   trayIcon.setImage(createImage("3.png"));
   }
   else if (test(4)) {
-   trayIcon = setIcon("4.png");
+   trayIcon.setImage(createImage("4.png"));
   }
   else if (test(5)) {
-   trayIcon = setIcon("5.png");
+   trayIcon.setImage(createImage("5.png"));
   }
   else if (test(10)) {
-   trayIcon = setIcon("10.png");
+   trayIcon.setImage(createImage("10.png"));
   } else if (test(100)) {
-   trayIcon = setIcon("100.png");
+   trayIcon.setImage(createImage("100.png"));
 
   } else if (test(1000)) {
-   trayIcon = setIcon("1000.png");
+   trayIcon.setImage(createImage("1000.png"));
   } else {
-   trayIcon = setIcon("unknown.png");
+   trayIcon.setImage(createImage("unknown.png"));
   }
-  return trayIcon;
+ }
+
+ public boolean isRunning() {
+  return running;
+ }
+
+ public void setRunning(boolean running) {
+  this.running = running;
  }
 }
